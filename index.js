41671dfda57fbae9ec4b056d6e47782f6eb41300
shani <shhhaniburde@gmail.com>
2021-12-15 12:10:11 -0500

    commit tree ba8d630f93255d5e8557255b278125d3c958b22b

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(drivers, name) {
+    const results = drivers.filter(name => name === drivers);
+    
+    if (name === drivers) {
+        return drivers.filter(name => name === drivers);
+    } else if (name === drivers.toUpperCase || drivers.toLowerCase) {
+        return drivers.filter(name => name === drivers);
+    }
+};
+findMatching();
